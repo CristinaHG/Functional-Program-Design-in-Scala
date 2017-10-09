29fe365fef7547fe59e54aefa5e017008cef07b4
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-10-09 16:42:26 +0200

    adding ifInsertAndDeleteThenEmpty propertie

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -29,11 +29,23 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  //If you insert any two elements into an empty heap, finding the minimum of the resulting heap should get the smallest of the two elements back.
-  property("minIsTheSmallerWhenTwo")=forAll { (a:Int, b:Int) =>
+  //Hint1: If you insert any two elements into an empty heap, finding the minimum of the resulting heap should get the smallest of the two elements back.
+  property("minIsTheSmallerWhenTwo") = forAll { (a:Int, b:Int) =>
     val h= insert(a,empty)
     val h2=insert(b,h)
     val min= if (a<=b) a else b
     findMin(h2)==min
   }
+
+  //Hint 2: If you insert an element into an empty heap, then delete the minimum, the resulting heap should be empty.
+  property("ifInsertAndDeleteThenEmpy") = forAll{ a:Int=>
+    val h=insert(a,empty)
+    val minRemoved= deleteMin(h)
+    isEmpty(minRemoved)==true
+  }
+
+  // Hint 3: Given any heap, you should get a sorted sequence of elements when continually finding and deleting minima. (Hint: recursion and helper functions are your friends.)
+
+  // Hint 4: Finding a minimum of the melding of any two heaps should return a minimum of one or the other.
+
 }
